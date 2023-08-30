1e2c8a5d2c4f57fcab9b6fbcf6fe2d7080426bf9
Wafie Abiyya <wafieabiyya7@gmail.com>
2023-08-30 15:43:24 +0700

    feature: price calculation

--- src/components/Fragments/Counter.jsx
+++ src/components/Fragments/Counter.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 
-class Counter extends React.Component{
-    constructor(props){
-        super(props);{
+class Counter extends React.Component {
+    constructor(props) {
+        super(props); {
             this.state = {
                 count: 0
             }
+            console.log('constructor')
+        }
+    }
+    componentDidMount() {
+        this.setState({ count: 1 })
+        console.log('componentDidMount')
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+        if(this.state.count === 10){
+            this.setState({count: 0})
+            console.log('componentUpdated')
         }
     }
     render() {
         return (
             <div className="flex justify-center bg-orange-500">
                 <h1 className="mx-3">{this.state.count}</h1>
-                <button className="bg-gray-800 text-white w-9" onClick={() => this.setState ({count: this.state.count +1})}>+</button>
+                <button className="bg-gray-800 text-white w-9" onClick={() => this.setState({ count: this.state.count + 1 })}>+</button>
+                {console.log('render')}
             </div>
         )
     }
